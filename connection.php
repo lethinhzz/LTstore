a77fe287499e4b9d86f91e16cdc4d97a9d681847
Lethinhz <133493415+Lethinhz@users.noreply.github.com>
2023-10-24 13:23:29 +0700

    upp

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','l9dce7widsrad87u','fuujdrls7rrfibc0','p8ojr1azcky1mfub')
+$conn = mysqli_connect('bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','h1gq4qcrxptols2z','han696y52zaaecvu','dsmqup4wjt33nt0x')
  or die("Can not connect database".mysqli_connect_error());
 ?>
